2c5d75baf025b6fb1ffe1bff6c38e00ea2c5b6a1
ThibaultMallevaey <tmalleva@gmail.com>
2024-11-11 19:11:27 +0100

    -

--- core/src/main/java/com/UE36/RpgM2/Screens/Map4.java
+++ core/src/main/java/com/UE36/RpgM2/Screens/Map4.java
@@ -4,15 +4,13 @@ import com.UE36.RpgM2.MainCharacter;
 import com.UE36.RpgM2.RpgGame;
 import com.UE36.RpgM2.Utilities.MapObjectRendering;
 import com.UE36.RpgM2.Utilities.Transitions;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
-public class Map3 extends RpgScreen{
+public class Map4 extends RpgScreen{
     private OrthographicCamera camera;
     private MainCharacter mainCharacter;
     private TiledMap map;
@@ -20,15 +18,15 @@ public class Map3 extends RpgScreen{
     private MapObjectRendering mapObjectRendering;
     private Transitions transitions;
 
-    public Map3(RpgGame game, Vector2 position) {
+
+    public Map4(RpgGame game, Vector2 position) {
         super(game, position);
         this.mainCharacter = game.getMainCharacter();
         setUpMainCharacter(mainCharacter, position, 500);
-        map = new TmxMapLoader().load("Maps/Carte3.tmx");
+        map = new TmxMapLoader().load("Maps/Carte4.tmx");
         mapObjectRendering = new MapObjectRendering(batch, map); // création de l'outil pour render la map
         mapRenderer = new OrthogonalTiledMapRenderer(map); //On définit le render sur orthogonal
     }
-
     @Override
     public void show() {
     }
@@ -36,10 +34,8 @@ public class Map3 extends RpgScreen{
     @Override
     protected void logic(){
         this.transitions = new Transitions(mainCharacter.getPosition(), map, mainCharacter);
-        if (transitions.onTransition("Lien_Carte2")){
-            game.setScreen(new Map2(game, new Vector2(1128, 900)));
-        } else if (transitions.onTransition("Lien_Carte4")){
-            game.setScreen(new Map4(game, new Vector2(20, 450)));
+        if (transitions.onTransition("Lien_Carte3")){
+            game.setScreen(new Map3(game, new Vector2(1550, 475)));
         }
     }
 
@@ -49,9 +45,8 @@ public class Map3 extends RpgScreen{
         basicRendering(delta, map, mapRenderer, mainCharacter);
         // initialisation du batch : render les objets
         batch.begin();
-        mapObjectRendering.renderLayerObjectsByTexture("arbre", "Arbre.png");
-        mapObjectRendering.renderLayerObjectsByTileId("Chimie1");
-        mapObjectRendering.renderLayerObjectsByTileId("minichateau");
+        mapObjectRendering.renderLayerObjectsByTexture("Arbre", "Arbre.png");
+        mapObjectRendering.renderLayerObjectsByTileId("PV");
         mainCharacter.render(batch); // render le perso
         batch.end();
 
